667bf2a4af4ccbcd53304a9a07c99ca4fa9bb056
Ömer Muhammed Gündoğdu <o.muhammed.gundogdu@gmail.com>
2021-11-03 02:10:31 +0300

    firebase eklentisi

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,15 @@ import firebase from "firebase";
 
 
 const firebaseApp = firebase.initializeApp( {
-  
+  // Projemize Firebase'i dahil edebilmek için oluşturulan config dosyasını yapıştırıyoruz.
+
+  //apiKey: ,
+  //authDomain: ,
+  //projectId: ,
+  //storageBucket: ,
+  //messagingSenderId: ,
+  //appId: ,
+  //measurementId: ,
 })
  
 const database = firebaseApp.firestore();
